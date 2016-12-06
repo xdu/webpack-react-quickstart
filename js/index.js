280c4969517522a775ac46e4f7ecae342e0878a3
unknown <bp367@IWIWKD012.win.iwi.local>
2016-12-06 16:30:47 +0100

    add router

--- js/index.js
+++ js/index.js
@@ -1,7 +1,65 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
+import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router'
 
-ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('root')
-);
+class Root extends React.Component {
+    render() {
+        return (
+            <div>
+                <div><Link to="/">Root</Link></div>
+                <ul>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/user">User</Link></li>
+                </ul>
+                {this.props.children}
+            </div>
+        )
+    }
+}
+
+class About extends React.Component {
+    render() {
+        return (
+            <div>
+                <div>About</div>
+                <span>location: {JSON.stringify(this.props.location)}</span><br/>
+            </div>
+        )
+    }
+}
+
+class User extends React.Component {
+    render() {
+        return (
+            <div>
+                <div>Client</div>
+                <span>location: {JSON.stringify(this.props.location)}</span><br/>
+                { this.props.children }
+            </div>
+        )
+    }
+}
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div>Home</div>
+        )
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return (
+            <Router history={hashHistory}>
+                <Route path="/" component={Root}>
+                    <IndexRoute component={Home} />
+                    <Route path="about" component={About} />
+                    <Route path="user" component={User} />
+                </Route>
+            </Router>
+        )
+    }
+}
+
+render(<App />, document.getElementById('root'));
